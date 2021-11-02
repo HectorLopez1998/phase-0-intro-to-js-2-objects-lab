a102912c3569c503d31bf9aaba9b7bfdb2312b83
hector <hectorlopezp1998@gmail.com>
2021-11-01 21:07:03 -0400

    commit tree 2822d3595057dcf0472653c45e2eb423b198344f

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your solution in this file!
+const employee = {
+	name: 'Hector',
+	streetAddress: '1234 str',
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+	const newOb = { ...employee }
+	newOb[key] = value
+
+	return newOb
+
+}
+
+updateEmployeeWithKeyAndValue()
